23e371d1818c05424aa27bf14286f7ef4bf1e364
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-28 14:57:34 -0700

    Move the two input functions into a different file, add the require logic for the setupInput function

--- play.js
+++ play.js
@@ -1,25 +1,10 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 
-const handleUserInput = function (data) {
-  process.stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.stdout.write('Terminated game\n');
-      process.exit();
-    }
-
-  })};
 
 setupInput();
 

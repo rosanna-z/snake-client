3cfdd2729ec7a43dfb81780c45ce85155b1bd822
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-28 16:22:36 -0700

    Add WASD keys to move snake, create new variable (connection) to equal conn

--- input.js
+++ input.js
@@ -1,5 +1,10 @@
+/* To allow the connection object to have access to the data 
+coming in from the keyboard. */
+let connection;
+
 //Function needed to use handleUserInput
-const setupInput = function () {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,16 +13,27 @@ const setupInput = function () {
   return stdin;
 };
 
-// When user pressed ctrl+c, the game will exit
-const handleUserInput = function (data) {
-  process.stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.stdout.write('Terminated game\n');
-      process.exit();
-    }
-
-  })};
-
-
+// When user presses ctrl+c, the game will exit
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.stdout.write('Terminated game\n');
+    process.exit();
+  }
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+  if (key === 'p') {
+    connection.write('Say: Haha!');
+  }
+};
 
 module.exports = { setupInput };

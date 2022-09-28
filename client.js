0000ce7e8ea85e2fefa3428e7c93988f26546c01
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-28 16:23:20 -0700

    Revised versin

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const net = require("net");
 
 // establishes a connection with the game server
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541
@@ -12,28 +12,15 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-    console.log("Successfully connected to game server!")
-    conn.write("Name: RZ")
-    conn.write("Move: down")
+    console.log("Successfully connected to game server!");
+    conn.write("Name: RZ");
+  });
 
-      setInterval(() => {
-      conn.write("Move: left");
-      }, 1000) 
-
-      setTimeout(() => {
-      conn.write("Move: up");
-      }, 1000);
-
-      setInterval(() => {
-        conn.write("Move: down");
-        }, 1000) 
-  
-
-    conn.on("data", (data) => {
-    console.log(data)
-    });
+  conn.on("data", (message) => {
+    console.log(message);
+  });
 
   return conn;
-})};
+};
 
 module.exports = { connect };

a33deda895e18581fb90c5f63e5daa043c8d22b1
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-28 14:49:16 -0700

    Two functions that closes the game if user presses ctrl+c

--- play.js
+++ play.js
@@ -3,3 +3,23 @@ const { connect } = require("./client");
 console.log("Connecting ...");
 connect();
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  process.stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.stdout.write('Terminated game\n');
+      process.exit();
+    }
+
+  })};
+
+setupInput();
+
